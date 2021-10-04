f191358524d332a4c1dce30adebffd746f6011d2
kolyamoloko <kolyamoloko@mail.ru>
2021-10-04 15:02:55 +0300

    add: season, transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function transform(arr) {
+
+  if(!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`)
+
+  let empty = []
+  let deleted
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++
+      deleted = i
+    } else if (arr[i] === '--discard-prev') {
+      if (i - 1 !== deleted) {
+        empty.pop()
+      } 
+    } else if (arr[i] === '--double-next') {
+      if(arr[i+1]) {
+        empty.push(arr[i+1])
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (i - 1 !== deleted && arr[i-1]) {
+        empty.push(arr[i-1])
+      }
+    } else {
+      empty.push(arr[i])
+    }
+  }
+  return empty
 }
--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ export default function getSeason(date) {
+  if (date == undefined) {
+    return "Unable to determine the time of year!";
+  } 
+  if (Object.prototype.toString.call(date) !== '[object Date]' || typeof date !== 'object' || Object.keys(date).length > 0 || !(date instanceof Date)) {
+    throw new Error('Invalid date!');
+  }
+    if (
+      (date.getMonth() >= 0 && date.getMonth() < 2) ||
+      date.getMonth() == 11
+    ) {
+      return "winter";
+    } else if (date.getMonth() >= 2 && date.getMonth() < 5) {
+      return "spring";
+    } else if (date.getMonth() >= 5 && date.getMonth() < 8) {
+      return "summer";
+    } else if (date.getMonth() >= 8 && date.getMonth() < 11) {
+      return "autumn";
+    }
+  } 
